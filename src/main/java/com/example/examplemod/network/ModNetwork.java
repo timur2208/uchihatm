bf7a6af51ce92e852cb0bacd5da4a92cee2132cc
Timur Karchigaev <timurkarcigaev605@gmail.com>
2025-12-06 17:19:02 +0300

    Fix ModNetwork.java API type

--- src/main/java/com/example/examplemod/network/ModNetwork.java
+++ src/main/java/com/example/examplemod/network/ModNetwork.java
@@ -2,12 +2,11 @@ package com.example.examplemod.network;
 
 import com.example.examplemod.UchihaTM;
 import net.neoforged.neoforge.network.event.RegisterPayloadHandlersEvent;
-import net.neoforged.neoforge.network.registration.PayloadRegistration;
 
 public class ModNetwork {
 
     public static void register(RegisterPayloadHandlersEvent event) {
-        PayloadRegistration registrar = event.registrar(UchihaTM.MODID);
+        var registrar = event.registrar(UchihaTM.MODID);
         registrar.playToServer(
                 SharinganTogglePacket.TYPE,
                 SharinganTogglePacket.CODEC,
